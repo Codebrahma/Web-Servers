e1f3edb1fe1dd11f145e2f2814ecdc1a77c6f75f
Anand Narayan <i@codeanand.com>
2014-04-05 13:52:31 +0530

    block http server

--- simple_blocking_tcp_server.rb
+++ simple_blocking_tcp_server.rb
@@ -2,13 +2,29 @@ require 'socket'
 require './app'
 server = TCPServer.new("0.0.0.0", 1234)
 
+def get_request(con)
+  response = ""
+  while current_line = con.recv(56)
+    response +=current_line
+    break if current_line.length < 56
+  end 
+  response
+end
 loop do
   connection = server.accept
   puts "connected with client"
-  inputline  = connection.gets
+  input  = get_request(connection)
+  puts "-------------------------------------"
+  puts "INPUT IS"
+  puts "#{input}"
 
-  output = App.call(inputline)
+  output = App.call(input)
 
-  connection.puts "Output is #{output}"
+  puts "-------------------------------------"
+  puts "OUTPUT is"
+  puts "#{output}"
+  puts "-------------------------------------"
+  connection.puts "#{output}"
   connection.close
 end
+
