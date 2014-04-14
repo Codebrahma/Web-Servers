11c2302e446796e65ff4f5874f8a09af3360ea4e
Anand Narayan <i@codeanand.com>
2014-04-14 15:55:05 +0530

    multi-threaded server

--- simple_blocking_tcp_server.rb
+++ simple_blocking_tcp_server.rb
@@ -2,9 +2,6 @@ require 'socket'
 require './app'
 number_of_process = 5
 server = TCPServer.new("0.0.0.0", 1234)
-(number_of_process-1).times do
-  break unless fork
-end
 
 def get_request(con)
   response = ""
@@ -19,22 +16,32 @@ def current_time
   Time.new.strftime("%H:%M:%S")
 end
 
-loop do
-  connection = server.accept
-    puts "connected with client at #{current_time}"
-    input  = get_request(connection)
-    puts "-------------------------------------"
-    puts "INPUT IS"
-    puts "#{input}"
+def start(server)
+  loop do
+    puts "server"
+    connection = server.accept
+      puts "connected with client at #{current_time}"
+      input  = get_request(connection)
+      puts "-------------------------------------"
+      puts "INPUT IS"
+      puts "#{input}"
 
-    output = App.call(input)
+      output = App.call(input)
 
-    puts "-------------------------------------"
-    puts "OUTPUT is"
-    puts "#{output}"
-    puts "processed at #{current_time}"
-    puts "-------------------------------------"
-    connection.puts "#{output}"
-    connection.close
+      puts "-------------------------------------"
+      puts "OUTPUT is"
+      puts "#{output}"
+      puts "processed at #{current_time}"
+      puts "-------------------------------------"
+      connection.puts "#{output}"
+      connection.close
+  end
 end
 
+(number_of_process-1).times do
+  Thread.new do
+    start(server)
+  end
+end
+
+gets
